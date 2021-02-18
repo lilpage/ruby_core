a7f666a44b5e013de81e346921bdbdc978bff2f8
Lil Page <lillehpage@gmail.com>
2021-02-17 22:29:03 -0600

    adds  email method

--- ruby_core_solution.rb
+++ ruby_core_solution.rb
@@ -1,12 +1,13 @@
 class Employee
-  attr_reader :first_name, :last_name, :salary, :active
-  attr_writer :first_name, :last_name, :salary, :active
+  attr_reader :first_name, :last_name, :email, :salary, :active
+  attr_writer :first_name, :last_name, :email, :salary, :active
 
-  def initialize(input_options = {first_name: "First", last_name: "Last", salary: 0, active: true})
+  def initialize(input_options = {first_name: "First", last_name: "Last", email: "default", salary: 0, active: true})
     @first_name = input_options[:first_name]
     @last_name = input_options[:last_name]
     @salary = input_options[:salary]
     @active = input_options[:active]
+    @email = input_options[:email]
   end
 
   def full_name
@@ -24,13 +25,25 @@ class Employee
   def give_annual_raise
     @salary = 1.05 * @salary
   end
+
+
+  def email
+    if @email == nil
+      puts "#{@first_name[0,5]}#{@last_name}@gmail.com"
+    else
+      puts @email
+    end
+  end
+
 end
 
-employee0 = Employee.new
-employee0.print_info
+
+# employee0 = Employee.new
+# employee0.print_info
 employee1 = Employee.new({first_name: "Majora", last_name: "Carter", salary: 80000, active: true})
 employee2 = Employee.new(first_name: "Danilo", last_name: "Campos", salary: 70000, active: true)
-employee1.print_info
-employee2.print_info
+# employee1.print_info
+# employee2.print_info
 puts employee1.full_name
 puts employee2.full_name
+employee1.email
