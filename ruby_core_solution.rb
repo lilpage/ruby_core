2a1be1a9104a0b11945510c4e52c81a00721d795
Lil Page <lillehpage@gmail.com>
2021-02-17 22:55:56 -0600

    Adds 100 identical drones to database

--- ruby_core_solution.rb
+++ ruby_core_solution.rb
@@ -10,6 +10,7 @@ class Employee
     @email = input_options[:email]
   end
 
+# part 1
   def full_name
     if @last_name[-1] == "s"
       return "#{@first_name} #{@last_name}, Esquire"
@@ -26,7 +27,7 @@ class Employee
     @salary = 1.05 * @salary
   end
 
-
+# part 3
   def email
     if @email == nil
       puts "#{@first_name[0,5]}#{@last_name}@gmail.com"
@@ -37,7 +38,7 @@ class Employee
 
 end
 
-  
+# calls part 2
 employee0 = Employee.new
 employee0.print_info
 employee1 = Employee.new({first_name: "Majora", last_name: "Carter", salary: 80000, active: true})
@@ -49,3 +50,18 @@ puts employee1.full_name
 puts employee2.full_name
 employee1.email
 employee3.email
+
+# stretch
+# Wow I guess someone hired 100 identical drones. Captialism huh?
+# employee_database = []
+# index = 0
+# while index < 100
+#   employee_database << Employee.new({first_name: "Drone Number", last_name: index+1, salary: 0, active: true})
+#   index += 1
+# end
+# # Print Empolyee Database
+# index = 0
+# while index < 100
+#   puts employee_database[index].full_name
+#   index +=1
+# end
