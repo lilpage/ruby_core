f44ee9cb3a17b5f483d34e8d2089115ad702882e
Lil Page <lillehpage@gmail.com>
2021-02-17 22:33:08 -0600

    New method calls

--- ruby_core_solution.rb
+++ ruby_core_solution.rb
@@ -37,13 +37,15 @@ class Employee
 
 end
 
-
-# employee0 = Employee.new
-# employee0.print_info
+  
+employee0 = Employee.new
+employee0.print_info
 employee1 = Employee.new({first_name: "Majora", last_name: "Carter", salary: 80000, active: true})
 employee2 = Employee.new(first_name: "Danilo", last_name: "Campos", salary: 70000, active: true)
-# employee1.print_info
-# employee2.print_info
+employee3 = Employee.new(first_name: "Chester", last_name: "Cheetah", email: "thecheesiest@gmail.com", salary: 1000000, active: true)
+employee1.print_info
+employee2.print_info
 puts employee1.full_name
 puts employee2.full_name
 employee1.email
+employee3.email
